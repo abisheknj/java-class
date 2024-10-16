7ec9c294f4d15515116073d48c3943f6edfccb31
abishekn1 <abishekraj0405@gmail.com>
2024-10-16 20:32:16 +0530

    first-commit

---
+++ VariableSession.java
@@ -0,0 +1,26 @@
+public class VariableSession{
+
+
+
+	public static void main(String[] args){
+
+
+		int age = 40;
+
+
+		System.out.println(age);
+		System.out.println(age);
+		System.out.println(age);
+
+
+
+
+
+
+	}
+	
+
+
+
+
+}
